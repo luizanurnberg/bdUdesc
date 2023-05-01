46c87e4bd276e884cab29f5265bc9607a0a310f7
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-05-01 16:14:45 -0300

    refactor: refeita a lista 4

--- 5-usandoAgregacao.sql
+++ 5-usandoAgregacao.sql
@@ -1,216 +1,229 @@
--- 1-) Utilizando o banco de dados de dvdrenral e os comandos de junção e função de
--- agregação, desenvolva consultas que respondam às questões a seguir
 -- a. Qual o número de locação?
-select count (*) from rental 
+SELECT COUNT (*) FROM rental;
 
 -- b. Qual o número de locações que foram pagas?
-select count (rental_id) from payment 
-
+SELECT COUNT (*) FROM rental AS rt
+	INNER JOIN payment AS pm
+	ON rt.rental_id = pm.rental_id;
+	
 -- c. Qual o valor total que a locadora movimentou?
-select sum (amount) from payment 
+SELECT SUM (pm.amount) FROM payment AS pm;
 
 -- d. Qual o valor total movimentado de 07/07/2005 até 23/08/2005?
-select sum (amount) from payment 
-	where payment_date between '2005/07/07' and '2005/08/23'
+SELECT SUM (pm.amount) FROM payment AS pm
+WHERE pm.payment_date BETWEEN '2005/07/07' AND '2005/08/23';
 	
 -- e. Qual a média do valor movimentado de 07/07/2005 até 23/08/2005?
-select avg (amount) from payment 
-	where payment_date between '2005/07/07' and '2005/08/23'
+SELECT AVG (pm.amount) FROM payment AS pm
+WHERE pm.payment_date BETWEEN '2005/07/07' AND '2005/08/23';
 	
 -- f. Quantos filmes existem por categoria?
-select sum (f.film_id), c.name from film as f
-	join film_category as fc
-		on f.film_id = fc.film_id
-	join category as c
-		on fc.category_id = c.category_id
-		 group by (c.name)
-		 order by (c.name)
+SELECT COUNT (fm.film_id), ct.name FROM film AS fm
+	INNER JOIN film_category AS fmct
+	ON fm.film_id = fmct.film_id
+		INNER JOIN category AS ct
+		ON ct.category_id = fmct.category_id
+		GROUP BY ct.name
+		ORDER BY ct.name;
 		 
 -- g. Quantos atores existem por categoria?
-select sum(ac.actor_id), c.name from film as f
-	join film_actor as fa
-		on fa.film_id = f.film_id
-	join actor as ac
-		on fa.actor_id = ac.actor_id
-	join film_category as fc
-		on f.film_id = fc.film_id
-	join category as c
-		on fc.category_id = c.category_id
-		 group by (c.name)
-		 order by (c.name)
+SELECT COUNT (ac.actor_id), ct.name FROM film AS fm
+	INNER JOIN film_category AS fmct
+	ON fm.film_id = fmct.film_id
+		INNER JOIN category AS ct
+		ON fmct.category_id = ct.category_id
+			INNER JOIN film_actor AS fmac
+				ON fm.film_id = fmac.film_id
+					INNER JOIN actor AS ac
+						ON fmac.actor_id = ac.actor_id
+						GROUP BY ct.name
+						ORDER BY ct.name;
 
 -- h. Quantos idiomas tem por filme?
-select sum (l.language_id), f.title from film as f
-	join language as l
-		on f.language_id = l.language_id
-	join film_category as fc
-		on f.film_id = fc.film_id
-		group by (f.title)
-		 order by (f.title)
+SELECT COUNT (language_id), fm.title FROM film AS fm
+GROUP BY fm.title;
 		 
 -- i. Quais atores fizeram mais filmes?
-select sum (f.film_id), ac.first_name, ac.last_name from film as f
-	join film_actor as fa
-		on fa.film_id = f.film_id
-	join actor as ac
-		on fa.actor_id = ac.actor_id
-		group by (ac.first_name, ac.last_name)
-		order by sum desc
-				
+SELECT COUNT (fm.film_id), ac.first_name FROM film AS fm
+	INNER JOIN film_actor AS fmac
+	ON fm.film_id = fmac.film_id
+		INNER JOIN actor AS ac
+		ON fmac.actor_id = ac.actor_id
+		GROUP BY ac.first_name
+		ORDER BY COUNT DESC
+		LIMIT 5;
+		
 -- j. Qual o número de cliente por cidade?
-select sum(co.customer_id), city from city as c 
-	inner join address as a  
-	on c.city_id = a.city_id
-	inner join customer as co
-	on a.address_id = co.address_id
-	group by (city)
+SELECT COUNT (cu.customer_id), cty.city FROM customer AS cu
+	INNER JOIN address AS adr
+	ON cu.address_id = adr.address_id
+		INNER JOIN city AS cty
+		ON adr.city_id = cty.city_id
+		GROUP BY city
+		ORDER BY COUNT DESC;
 		
 -- k. Qual a quantidade de filme por categoria?
-select sum (f.film_id), c.name from film as f
-	join film_category as fc
-		on f.film_id = fc.film_id
-	join category as c
-		on fc.category_id = c.category_id
-		 group by (c.name)
-		 order by (c.name)
+SELECT COUNT (fm.film_id), ct.name FROM film AS fm
+	INNER JOIN film_category AS fmct
+	ON fm.film_id = fmct.film_id
+		INNER JOIN category AS ct
+		ON fmct.category_id = ct.category_id
+			GROUP BY ct.name
+			ORDER BY COUNT DESC;
 		 
 -- l. Quais foram as 10 categorias mais locadas?
-select sum(rental_id), ca.name from rental as r 
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film_category as fc
-	on i.film_id = fc.film_id
-	inner join category as ca 
-	on fc.category_id = ca.category_id
-	group by (ca.name)
-	order by sum desc limit 10
+SELECT COUNT (ct.category_id), ct.name FROM category AS ct
+	INNER JOIN film_category AS fmct
+	ON ct.category_id = fmct.category_id
+		INNER JOIN film AS fm
+		ON fmct.film_id = fm.film_id
+			INNER JOIN inventory AS invt
+			ON fm.film_id = invt.film_id
+				INNER JOIN rental AS rt
+				ON rt.inventory_id = invt.inventory_id
+				GROUP BY ct.name 
+				ORDER BY COUNT DESC
+				LIMIT 10;
 	
 -- m. Quais foram as 10 categorias mais rentáveis?
-select sum (amount), ca.name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film_category as fc
-	on i.film_id = fc.film_id
-	inner join category as ca 
-	on fc.category_id = ca.category_id
-	group by (ca.name)
-	order by sum desc limit 10
+SELECT COUNT (ct.category_id), ct.name FROM category AS ct
+	INNER JOIN film_category AS fmct
+	ON ct.category_id = fmct.category_id
+		INNER JOIN film AS fm
+		ON fmct.film_id = fm.film_id
+			INNER JOIN inventory AS invt
+			ON fm.film_id = invt.film_id
+				INNER JOIN rental AS rt
+				ON rt.inventory_id = invt.inventory_id
+					INNER JOIN payment AS pm
+					ON pm.rental_id = rt.rental_id
+					GROUP BY ct.name 
+					ORDER BY COUNT DESC
+					LIMIT 10;
+					
 	
 -- n. Quais foram os 10 filmes mais locados?
-select sum(rental_id), f.title from rental as r
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film as f
-	on i.film_id = f.film_id
-	group by(f.title)
-	order by sum desc limit 10
+SELECT COUNT (fm.film_id), fm.title FROM film AS fm
+	INNER JOIN inventory AS invt
+	ON fm.film_id = invt.film_id
+		INNER JOIN rental AS rt
+		ON rt.inventory_id = invt.inventory_id
+		GROUP BY fm.title 
+		ORDER BY COUNT DESC
+		LIMIT 10;
 	
 -- o. Quais foram os 10 filmes mais rentáveis?
-select sum (amount), ca.name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film_category as fc
-	on i.film_id = fc.film_id
-	inner join category as ca 
-	on fc.category_id = ca.category_id
-	group by (ca.name)
-	order by sum desc limit 10
+SELECT COUNT (fm.film_id), fm.title FROM film AS fm
+	INNER JOIN inventory AS invt
+	ON fm.film_id = invt.film_id
+		INNER JOIN rental AS rt
+		ON rt.inventory_id = invt.inventory_id
+			INNER JOIN payment AS pm
+			ON pm.rental_id = rt.rental_id
+			GROUP BY fm.title 
+			ORDER BY COUNT DESC
+			LIMIT 10;
 	
 -- p. Quais foram os 10 filmes menos locados?
-select sum(rental_id), f.title from rental as r
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film as f
-	on i.film_id = f.film_id
-	group by(f.title)
-	order by sum asc limit 10
+SELECT COUNT (fm.film_id), fm.title FROM film AS fm
+	INNER JOIN inventory AS invt
+	ON fm.film_id = invt.film_id
+		INNER JOIN rental AS rt
+		ON rt.inventory_id = invt.inventory_id
+		GROUP BY fm.title 
+		ORDER BY COUNT ASC
+		LIMIT 10;
 	
 -- q. Quais foram os 10 filmes menos rentáveis?
-select sum (amount), ca.name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join inventory as i
-	on r.inventory_id = i.inventory_id 
-	inner join film_category as fc
-	on i.film_id = fc.film_id
-	inner join category as ca 
-	on fc.category_id = ca.category_id
-	group by (ca.name)
-	order by sum asc limit 10
+SELECT COUNT (fm.film_id), fm.title FROM film AS fm
+	INNER JOIN inventory AS invt
+	ON fm.film_id = invt.film_id
+		INNER JOIN rental AS rt
+		ON rt.inventory_id = invt.inventory_id
+			INNER JOIN payment AS pm
+			ON pm.rental_id = rt.rental_id
+			GROUP BY fm.title 
+			ORDER BY COUNT ASC
+			LIMIT 10;
 	
 -- r. Quais os 10 clientes que mais alugam filmes?
-select sum(rental_id), c.first_name from rental as r
-	inner join customer as c
-	on r.customer_id = c.customer_id
-	group by (c.first_name)
-	order by sum desc limit 10
+SELECT COUNT (rt.rental_id), ctm.first_name FROM customer AS ctm
+	INNER JOIN rental AS rt
+	ON rt.customer_id = ctm.customer_id
+	GROUP BY ctm.first_name
+	ORDER BY COUNT DESC
+	LIMIT 10;
 	
 -- s. Quais os 10 clientes que mais dão lucro?
-select sum (amount), c.first_name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join customer as c
-	on r.customer_id = c.customer_id
-	group by (c.first_name)
-	order by sum desc limit 10
+SELECT SUM (amount), ctm.first_name FROM customer AS ctm
+	INNER JOIN rental AS rt
+	ON rt.customer_id = ctm.customer_id
+		INNER JOIN payment AS pm
+		ON pm.rental_id = rt.rental_id
+		GROUP BY ctm.first_name
+		ORDER BY SUM DESC
+		LIMIT 10;
 	
 -- t. Quais os 10 clientes que menos alugam filmes?
-select sum(rental_id), c.first_name from rental as r
-	inner join customer as c
-	on r.customer_id = c.customer_id
-	group by (c.first_name)
-	order by sum asc limit 10
+SELECT COUNT (rt.rental_id), ctm.first_name FROM customer AS ctm
+	INNER JOIN rental AS rt
+	ON rt.customer_id = ctm.customer_id
+	GROUP BY ctm.first_name
+	ORDER BY COUNT ASC
+	LIMIT 10;
 	
 -- u. Quais os 10 clientes que menos dão lucro?
-select sum (amount), c.first_name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join customer as c
-	on r.customer_id = c.customer_id
-	group by (c.first_name)
-	order by sum asc limit 10
+SELECT SUM (pm.amount), ctm.first_name FROM customer AS ctm
+	INNER JOIN rental AS rt
+	ON rt.customer_id = ctm.customer_id
+		INNER JOIN payment AS pm
+		ON pm.rental_id = rt.rental_id
+		GROUP BY ctm.first_name
+		ORDER BY SUM ASC
+		LIMIT 10;
 	
 -- v. Quais os 10 funcionários que mais alugam filmes?
-select sum(rental_id), s.first_name from rental as r
-	inner join staff as s
-	on r.staff_id = s.staff_id
-	group by (s.first_name)
-	order by sum desc limit 10
+SELECT COUNT (rt.rental_id), st.first_name FROM rental AS rt
+	INNER JOIN staff AS st
+	ON rt.staff_id = st.staff_id
+	GROUP BY st.first_name
+	ORDER BY COUNT DESC
+	LIMIT 10;
 	
 -- w. Quais os 10 funcionários que mais dão lucro?
-select sum (amount), s.first_name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join staff as s
-	on r.staff_id = s.staff_id
-	group by (s.first_name)
-	order by sum desc limit 10
+SELECT SUM (pm.amount), st.first_name FROM rental AS rt
+	INNER JOIN payment AS pm
+	ON rt.rental_id = pm.rental_id
+		INNER JOIN staff AS st
+		ON pm.staff_id = st.staff_id
+		GROUP BY st.first_name
+		ORDER BY SUM DESC
+		LIMIT 10;
 	
 -- x. Quais os 10 funcionários que menos alugam filmes?
-select sum(rental_id), c.first_name from rental as r
-	inner join customer as c
-	on r.customer_id = c.customer_id
-	group by (c.first_name)
-	order by sum asc limit 10
+SELECT COUNT (rt.rental_id), st.first_name FROM rental AS rt
+	INNER JOIN staff AS st
+	ON rt.staff_id = st.staff_id
+	GROUP BY st.first_name
+	ORDER BY COUNT ASC
+	LIMIT 10;
 	
 -- y. Quais os 10 funcionários que menos dão lucro?
-select sum (amount), s.first_name from payment as p
-	inner join rental as r
-	on p.rental_id = r.rental_id
-	inner join staff as s
-	on r.staff_id = s.staff_id
-	group by (s.first_name)
-	order by sum asc limit 10
+SELECT SUM (pm.amount), st.first_name FROM rental AS rt
+	INNER JOIN payment AS pm
+	ON rt.rental_id = pm.rental_id
+		INNER JOIN staff AS st
+		ON pm.staff_id = st.staff_id
+		GROUP BY st.first_name
+		ORDER BY SUM ASC
+		LIMIT 10;
 	
 -- z. Quais os 10 endereços onde mais residem pessoas?
-select sum(co.customer_id), city from city as c 
-	inner join address as a  
-	on c.city_id = a.city_id
-	inner join customer as co
-	on a.address_id = co.address_id
-	group by (city)
-	order by sum desc limit 10
+SELECT COUNT (adr.address_id), cty.city FROM customer AS cu
+	INNER JOIN address AS adr
+	ON cu.address_id = adr.address_id
+		INNER JOIN city AS cty
+		ON adr.city_id = cty.city_id
+		GROUP BY city
+		ORDER BY COUNT DESC
+		LIMIT 10;
